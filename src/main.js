9d5923d6721156a67936b547e04a9707624319c7
ChaBen <ckqlss@gmail.com>
2018-10-11 00:59:36 +0900

    init vuetify

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import Vuetify from 'vuetify'
+
+// css
+import 'vuetify/dist/vuetify.min.css'
+
+Vue.use(Vuetify)
 
 Vue.config.productionTip = false
 
